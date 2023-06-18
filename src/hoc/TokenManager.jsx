ba2502784dccf786ac0eee39f151bf4072ee242f
Abilmansur Yunis <43860831+abilmansur4@users.noreply.github.com>
2023-06-19 01:42:58 +0500

    Add files via upload

---
+++ src/hoc/TokenManager.jsx
@@ -0,0 +1,24 @@
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+
+const TokenManager = () => {
+  const updateToken = () => {
+    axios.get("http://localhost:5000/api/auth/refresh")
+      .then((response) => {
+        localStorage.setItem("accessToken", response.data.accessToken);
+        localStorage.setItem("refreshToken", response.data.refreshToken);
+      })
+  };
+
+  useEffect(() => {
+    updateToken();
+    // Установите интервал обновления токена в соответствии с вашими требованиями
+    const interval = setInterval(updateToken, 1800000); // Обновление токена каждый час
+
+    // Очистка интервала при размонтировании компонента
+    return () => clearInterval(interval);
+  }, []);
+
+};
+
+export default TokenManager;
